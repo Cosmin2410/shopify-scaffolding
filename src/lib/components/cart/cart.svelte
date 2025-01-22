19a5fa9832159c0fd8783a42d17ff378bf3f5e51
Cosmin2410 <naecosmin@Naes-MacBook-Pro-2.local>
2025-01-22 20:59:10 +0200

    checkout changes

--- src/lib/components/cart/cart.svelte
+++ src/lib/components/cart/cart.svelte
@@ -55,64 +55,74 @@
 	<Sheet.Content class="overflow-y-auto">
 		<Sheet.Header>
 			<Sheet.Title>Cart</Sheet.Title>
+		</Sheet.Header>
 
-			{#each $cart?.lines?.edges as line}
-				<div class="rounded bg-secondary p-4">
-					<Trash
-						class="ml-auto h-5 w-5 cursor-pointer text-red-400"
-						onclick={() =>
-							handleCartLineUpdate(1, line?.node?.merchandise?.id, line?.node, 'remove-all')}
-					/>
+		<div class="mt-6">
+			{#if $cart?.lines?.edges?.length}
+				{#each $cart?.lines?.edges as line}
+					<div class="mb-4 rounded bg-secondary p-4">
+						<Trash
+							class="ml-auto h-5 w-5 cursor-pointer text-red-400"
+							onclick={() =>
+								handleCartLineUpdate(1, line?.node?.merchandise?.id, line?.node, 'remove-all')}
+						/>
 
-					<div>
-						<p>
-							{line?.node?.merchandise?.product?.title}
-						</p>
-
-						<p>
-							Quantity: {loading && productId === line?.node?.merchandise?.id
-								? 'Loading...'
-								: line?.node?.quantity}
-						</p>
-					</div>
-
-					<div class="mt-4 border-t border-secondary-foreground">
-						<div class="mt-4 flex items-center justify-between">
-							<div>
-								<Button
-									class="text-lg"
-									onclick={() =>
-										handleCartLineUpdate(1, line?.node?.merchandise?.id, line?.node, 'retract')}
-									disabled={loading && productId === line?.node?.merchandise?.id}
-								>
-									-
-								</Button>
-								<Button
-									class="text-lg"
-									onclick={() =>
-										handleCartLineUpdate(1, line?.node?.merchandise?.id, line?.node, 'add')}
-									disabled={loading && productId === line?.node?.merchandise?.id}
-								>
-									+
-								</Button>
-							</div>
+						<div>
+							<p>
+								{line?.node?.merchandise?.product?.title}
+							</p>
 
 							<p>
-								{line?.node?.merchandise?.product?.priceRange?.maxVariantPrice?.amount}
-								{line?.node?.merchandise?.product?.priceRange?.maxVariantPrice?.currencyCode}
+								Quantity: {loading && productId === line?.node?.merchandise?.id
+									? 'Loading...'
+									: line?.node?.quantity}
 							</p>
 						</div>
-					</div>
-				</div>
-			{/each}
 
-			<div class="flex items-center justify-between">
-				<p class="mt-4 font-medium">Total:</p>
-				<p class="mt-4 font-bold">
-					{$cart?.cost?.checkoutChargeAmount?.amount}
-					{$cart?.cost?.checkoutChargeAmount?.currencyCode}
-				</p>
-			</div>
-		</Sheet.Header>
+						<div class="mt-4 border-t border-secondary-foreground">
+							<div class="mt-4 flex items-center justify-between">
+								<div>
+									<Button
+										class="text-lg"
+										onclick={() =>
+											handleCartLineUpdate(1, line?.node?.merchandise?.id, line?.node, 'retract')}
+										disabled={loading && productId === line?.node?.merchandise?.id}
+									>
+										-
+									</Button>
+									<Button
+										class="text-lg"
+										onclick={() =>
+											handleCartLineUpdate(1, line?.node?.merchandise?.id, line?.node, 'add')}
+										disabled={loading && productId === line?.node?.merchandise?.id}
+									>
+										+
+									</Button>
+								</div>
+
+								<p>
+									{line?.node?.merchandise?.product?.priceRange?.maxVariantPrice?.amount}
+									{line?.node?.merchandise?.product?.priceRange?.maxVariantPrice?.currencyCode}
+								</p>
+							</div>
+						</div>
+					</div>
+				{/each}
+
+				<div class="flex items-center justify-between">
+					<p class="mt-4 font-medium">Total:</p>
+					<p class="mt-4 font-bold">
+						{$cart?.cost?.checkoutChargeAmount?.amount || 0}
+						{$cart?.cost?.checkoutChargeAmount?.currencyCode}
+					</p>
+				</div>
+
+				<div class="flex items-center justify-center">
+					<Button href={$cart?.checkoutUrl} class="mt-6">Proceed to Checkout</Button>
+				</div>
+			{:else}
+				<p class="flex items-center justify-center">No items added</p>
+			{/if}
+		</div>
 	</Sheet.Content>
 </Sheet.Root>
--- src/routes/checkout/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	import { cart } from '$lib/store/cart';
-	import { Button } from '$lib/components/ui/button/index.js';
-</script>
-
-<Button href={$cart?.checkoutUrl}>Proceed to Checkout</Button>
